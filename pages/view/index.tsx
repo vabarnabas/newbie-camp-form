c065eabfc9765a4fb341b96bdab2adb56777e32c
Barnabas Varga <vabarnabas@gmail.com>
2022-09-27 18:41:52 +0200

    CSV

--- pages/view/index.tsx
+++ pages/view/index.tsx
@@ -13,6 +13,8 @@ import { getTickets } from "../../services/api/getTickets"
 import { updateForm } from "../../services/updateForm"
 import { useAutoAnimate } from "@formkit/auto-animate/react"
 import { Answer } from "../../types/answers.types"
+import { CSVLink, CSVDownload } from "react-csv"
+import { generateCSVData } from "../../services/generateCSVData"
 
 const Page: NextPage = () => {
   const { formStorage, modifyStorage } = useFormStorage()
@@ -41,8 +43,22 @@ const Page: NextPage = () => {
         <Form hide onSubmit={() => {}} title="Jegyek">
           <div ref={ref} className="space-y-3">
             <div className="">
-              <p className="text-xl font-bold">{`${answers.length} Jelentkezés`}</p>
-              <div className="flex flex-col">
+              <div className="flex items-center justify-between">
+                <p className="text-xl font-bold">{`${answers.length} Jelentkezés`}</p>
+                <CSVLink
+                  data={generateCSVData(
+                    answers
+                      .filter((answer) => answer.isActive === true)
+                      .map((answer) => {
+                        return JSON.parse(answer.formValues)
+                      })
+                  )}
+                  className="rounded-md bg-soft-green py-1 px-3 text-sm text-white outline-none hover:bg-soft-green-dark disabled:bg-slate-400"
+                >
+                  Letöltés
+                </CSVLink>
+              </div>
+              <div className="mt-2 flex flex-col">
                 {[
                   ...new Map(
                     answers
--- services/generateCSVData.ts
+++ services/generateCSVData.ts
@@ -1,10 +1,18 @@
 import { FormValues } from "../types/formvalues.types"
 
-export const generateCSVData = (formValues: FormValues) => {
-  return {
-    fullName: formValues.fullName,
-    email: formValues.email,
-    neptunCode: formValues.neptunCode,
-    
-  }
+export const generateCSVData = (data: FormValues[]) => {
+  return data.map((formValues) => {
+    return {
+      fullName: formValues.fullName,
+      email: formValues.email,
+      neptunCode: formValues.neptunCode,
+      address: `${formValues.zipCode} ${formValues.city}, ${formValues.address}`,
+      placeOfBirth: formValues.placeOfBirth,
+      dateOfBirth: formValues.dateOfBirth,
+      idNumber: formValues.idNumber,
+      studentIdNumber: formValues.studentIdNumber,
+      ticket: formValues.ticket?.displayName,
+      nightStay: formValues?.nightStay,
+    }
+  })
 }
